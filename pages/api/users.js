937640be9d6d89084c585f825f4651be0dc48ca1
Oluwatobi Adegoke <adegoketobi5496@gmail.com>
2021-08-12 14:43:42 +0100

    user api route added

---
+++ pages/api/users.js
@@ -0,0 +1,34 @@
+import { connectToDatabase } from "../../helpers/database";
+
+export default function handler(req, res) {
+  if (req.method !== "GET") {
+    res.status(422).json({ response: "0", message: "Method not supported" });
+    return;
+  }
+
+  let client;
+  try {
+    client = await connectToDatabase();
+  } catch (error) {
+    res
+      .status(500)
+      .json({ response: "0", message: "Error connecting to database" });
+    return;
+  }
+
+  try {
+    const db = client.db("xpress");
+    const users = await db.collection("users").find({}).toArray();
+    client.close();
+    res.status(200).json({
+      response: "1",
+      message: "Users fetched successfully",
+      data: users,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ response: "0", message: "Error while fetching post" });
+    return;
+  }
+}
